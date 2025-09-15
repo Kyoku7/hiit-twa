b2c03215fb90f3bdeab8f4af5977308e76aa7479
Kyoku7 <juanjoafonso7@gmail.com>
2025-09-15 23:08:09 +0000

    Progreso empaquetado TWA

---
+++ app/src/main/java/app/netlify/nimble_fenglisu_8f9960/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.nimble_fenglisu_8f9960.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
